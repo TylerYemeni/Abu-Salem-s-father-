9823d2d51272ab6ad28c62f90748a6f315427768
TylerYemeni <leomessi111909@gmail.com>
2025-04-18 07:58:17 +0300

    Update CellAnalyzer.kt

--- CellAnalyzer.kt
+++ CellAnalyzer.kt
@@ -1,40 +1,51 @@
-package com.abusalem.netsentinel.service
+import com.abusalem.netsentinel.utils.RootActions
 
-import android.content.Context
-import android.telephony.*
-import android.util.Log
-import java.io.File
-import java.io.FileWriter
+...
 
-object CellAnalyzer {
+fun analyze(context: Context) {
+    val tm = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+    val cells = tm.allCellInfo
+    val file = File(context.filesDir, "cell_towers_log.csv")
+    val writer = FileWriter(file, true)
 
-    fun analyze(context: Context) {
-        val tm = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-        val cells = tm.allCellInfo
-        val file = File(context.filesDir, "cell_towers_log.csv")
-        val writer = FileWriter(file, true)
-
-        for (info in cells) {
-            if (info is CellInfoGsm) {
-                val cid = info.cellIdentity.cid
-                val lac = info.cellIdentity.lac
-                val mcc = info.cellIdentity.mcc
-                val mnc = info.cellIdentity.mnc
-                val dbm = info.cellSignalStrength.dbm
-
-                val line = "${System.currentTimeMillis()},GSM,$mcc,$mnc,$lac,$cid,$dbm\n"
-                writer.write(line)
-                Log.d("CellAnalyzer", line)
-            }
-            if (info is CellInfoLte) {
-                val cell = info.cellIdentity
-                val signal = info.cellSignalStrength
-                val line = "${System.currentTimeMillis()},LTE,${cell.mcc},${cell.mnc},${cell.tac},${cell.ci},${signal.dbm}\n"
-                writer.write(line)
-                Log.d("CellAnalyzer", line)
-            }
+    for (info in cells) {
+        val type = if (info is CellInfoGsm) "GSM" else if (info is CellInfoLte) "LTE" else "UNKNOWN"
+        val mcc = when (info) {
+            is CellInfoGsm -> info.cellIdentity.mcc
+            is CellInfoLte -> info.cellIdentity.mcc
+            else -> -1
+        }
+        val mnc = when (info) {
+            is CellInfoGsm -> info.cellIdentity.mnc
+            is CellInfoLte -> info.cellIdentity.mnc
+            else -> -1
+        }
+        val cid = when (info) {
+            is CellInfoGsm -> info.cellIdentity.cid
+            is CellInfoLte -> info.cellIdentity.ci
+            else -> -1
+        }
+        val lac = when (info) {
+            is CellInfoGsm -> info.cellIdentity.lac
+            is CellInfoLte -> info.cellIdentity.tac
+            else -> -1
+        }
+        val signal = when (info) {
+            is CellInfoGsm -> info.cellSignalStrength.dbm
+            is CellInfoLte -> info.cellSignalStrength.dbm
+            else -> 0
         }
 
-        writer.close()
+        val root = if (RootActions.isRooted()) "Rooted" else "NoRoot"
+        val extraRootInfo = if (RootActions.isRooted()) {
+            RootActions.runAsRoot("dumpsys telephony.registry | grep cell")
+        } else {
+            "No Root Access"
+        }
+
+        val log = "${System.currentTimeMillis()},$type,$mcc,$mnc,$lac,$cid,$signal,$root,${extraRootInfo.replace("\n", "|")}\n"
+        writer.write(log)
     }
+
+    writer.close()
 }
